1d5e49e358b46efa4ac7ed161a7d1a5f8d78bc8a
Toe-Tow <62809444+Toe-Tow@users.noreply.github.com>
2021-03-11 19:06:42 -0500

    Add files via upload

---
+++ Portfolio1/src/App.js
@@ -0,0 +1,18 @@
+
+import Reps from './components/Reps/Reps';
+import Laps from './components/Laps/Laps';
+import Jumbo from './components/Jumbo/Jumbo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <Jumbo/>
+    <Reps/>
+    <Laps/>
+    </div>
+  );
+}
+
+export default App;
